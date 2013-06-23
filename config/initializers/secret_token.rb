1d35a116df9eee7f5d04e93d74df02bc871c23ce
Emily Wright-Moore <ewmoore@EWMOORE.local>
2013-06-23 10:02:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Payphones::Application.config.secret_token = 'c9e45bd82d3f93528c27fdcf58bc75eab6eea09922b94ac718e66d1bf7eeb318397380d52a8dd6a2c478bb63121b84566be62610cbb846d3e828e279264c5156'
