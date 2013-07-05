9070c39aa5a9bd371108131f9808e09e15fe8a0e
Emily Wright-Moore <ewmoore@EWMOORE.local>
2013-07-05 16:59:44 -0700

    remove random html/js file

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,7 @@
-mapbox.load('layerid', function(o) {
-    var map = mapbox.map('map');
-    map.addLayer(o.layer).zoom(o.zoom).center(o.center);
-    map.ui.zoomer.add();
-    map.ui.zoombox.add();
-    map.interaction.auto();
-});
+// mapbox.load('layerid', function(o) {
+//     var map = mapbox.map('map');
+//     map.addLayer(o.layer).zoom(o.zoom).center(o.center);
+//     map.ui.zoomer.add();
+//     map.ui.zoombox.add();
+//     map.interaction.auto();
+// });
