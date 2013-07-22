c6055e01c5a730d096850d6e589286678dba88e2
Jeremy Moore <jeremymoore@gmail.com>
2013-07-21 22:42:09 -0700

    don't try to parse redis uri

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-redis_uri = URI.parse(ENV["REDIS_URL"])
-$redis = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
+# uri = URI.parse("redis://rediscloud:D2tfqczAyZTEBrBO@pub-redis-18334.us-east-1-4.2.ec2.garantiadata.com:18334")
+$redis = Redis.new(:host => ENV["REDIS_HOST"], :port => ENV["REDIS_PORT"], :password => ENV["REDIS_PASSWORD"])
