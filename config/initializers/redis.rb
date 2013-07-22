dcfe96901acedc155a49ee05062b4f84495a46fe
Jeremy Moore <jeremymoore@gmail.com>
2013-07-21 21:58:58 -0700

    redis please

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-redis_uri = URI.parse(URI.encode(ENV['REDISCLOUD_URL']))
+redis_uri = URI.parse(ENV['REDISCLOUD_URL'])
 $redis = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
