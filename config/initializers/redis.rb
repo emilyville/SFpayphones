8daa8ef4f9a0476eb7616ee983a4393dff7165fe
Jeremy Moore <jeremymoore@gmail.com>
2013-07-21 22:51:31 -0700

    revert back

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-# uri = URI.parse("redis://rediscloud:D2tfqczAyZTEBrBO@pub-redis-18334.us-east-1-4.2.ec2.garantiadata.com:18334")
-$redis = Redis.new(:host => ENV["REDIS_HOST"], :port => ENV["REDIS_PORT"], :password => ENV["REDIS_PASSWORD"])
+uri = URI.parse(ENV["REDISCLOUD_URL"])
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
