5c463056bbf0af45a528c20ff83714176bbb5245
Jeremy Moore <jeremymoore@gmail.com>
2013-08-03 21:47:23 -0700

    update main controller for resqueue

--- app/controllers/callbox_controller.rb
+++ app/controllers/callbox_controller.rb
@@ -60,7 +60,7 @@ class CallboxController < ApplicationController
 	def caller_connected
 		root_callsid = params[:CallSid]
 		logger.info "scheduling background job to create calls"
-		BackgroundCaller.perform_async root_callsid
+		Resque.enqueue BackgroundCaller.new(root_callsid)
 		logger.info "adding caller to queue"
 		# put caller in queue
 		begin
@@ -154,30 +154,5 @@ class CallboxController < ApplicationController
 			logger.info "error cleaning up call:#{number}, #{e.message}"
 		end
 		render :nothing => true
-	end
-
-	class BackgroundCaller
-
-		def perform root_callsid
-			pay_phones = $redis.hgetall "#{root_callsid}-outgoing"
-			logger.info "starting calls"
-			pay_phones.keys.each do |payphone|
-				begin
-					call = $twilio.account.calls.create(
-						:From => '+14155086687',
-						:To => payphone,
-						:Url => url_for(controller: 'callbox', action: 'call_connected'),
-						:StatusCallback => url_for(controller: 'callbox', action: 'call_completed')
-						)
-				rescue Exception => e
-					logger.info "Unable to initiate call to #{payphone}: #{e.message}"
-				end
-				$redis.hset "#{root_callsid}-outgoing", call.to, call.sid
-				$redis.set "#{call.sid}-root", root_callsid
-				$redis.hsetnx "phonestatus", call.to, "ringing"
-				$redis.expire "phonestatus", 180
-				$redis.publish "callsupdated", JSON.dump({:status => "ringing"})
-			end
-		end
 	end
 end
