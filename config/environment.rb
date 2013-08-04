12f71f83a8cc2065a8276d16e9c3237f56276965
Jeremy Moore <jeremymoore@gmail.com>
2013-08-03 22:52:15 -0700

    remove old config stting, initialize logger

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 Rails.logger = Logger.new(STDOUT)
-
+Resque.logger = MyLogger.new(STDOUT)
 # Initialize the rails application
 Payphones::Application.initialize!
